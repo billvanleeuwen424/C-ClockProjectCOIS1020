44578992c54b259607888498de9c95ee8f90868d
unknown <billvanleeuwen424@gmail.com>
2021-03-21 13:03:38 -0400

    Bug checked. All Properties, and all non overrider methods are working. demo contains short demo

--- Clock.cs
+++ Clock.cs
@@ -12,7 +12,7 @@ public class Clock
         //set default. 12:00AM
         hours = 12;
         minutes = 0;
-        clockType = 'a';
+        clockType = 'A';
     }
 
     // Three Parameter Constructor
@@ -27,13 +27,16 @@ public class Clock
     // Hours Property
     public int Hours
     {
-        get;    //default getter
+        get 
+        {
+            return hours;
+        }
 
         set
         {
-            if (ClockType == 'm')
+            if (ClockType == 'M')
             {
-                if (value <= 23 && hours >= 0)  //checks if hours are valid 0 - 23 for military
+                if (value <= 23 && value >= 0)  //checks if hours are valid 0 - 23 for military
                     hours = value;
                 else
                 {
@@ -43,7 +46,7 @@ public class Clock
             }
             else
             {
-                if (value <= 12 && hours > 0)   //checks if hours are valid 1 - 12 for standard time
+                if (value <= 12 && value > 0)   //checks if hours are valid 1 - 12 for standard time
                     hours = value;
                 else
                 {
@@ -57,7 +60,11 @@ public class Clock
     // Minutes Property
     public int Minutes
     {
-        get;
+        get 
+        {
+            return minutes;
+        }
+
         set
         {
             if (value < 59 && value >= 0)   //checks if mintues are valid. 0-59
@@ -74,39 +81,80 @@ public class Clock
     //Clock type property
     public char ClockType
     {
-        get;    //default getter
+        get 
+        {
+            return clockType;
+        }    
 
         set //checks if value is correct, else sets type to AM
         {
             if (value == 'A' || value == 'P' || value == 'M')
                 clockType = value;
             else
+            {
                 clockType = 'A';
+                Console.WriteLine("invalid time entered... Clocktype set to AM");
+            }
         }
     }
 
     //ConvertToMilitary method
+    //depending on the clocktype, the hours will be changed into military format. Since the minutes are in the same format, those do not need to be touched
     private void ConvertToMilitary()
     {
-        // insert code here
+        int totalHour = 0;  //value to hold hours while converting
+
+        // if the clocktype is already military, nothing will happen
+        if (clockType != 'M')
+        {
+            if (clockType == 'P')
+            {
+                totalHour = 12;
+                totalHour += hours;
+                hours = totalHour;
+            }
+            else    //when the clock is am
+                if (hours == 12)    //if the clock is am, and the hours are 12, in military time that is 00
+                    hours = 0;
+                else    //otherwise can just convert hours to hours
+                    totalHour = hours;
+                
+        }
+        //after hours are changed, clocktype is switched to m
+        clockType = 'M';
+
     }
 
-    //ConvertToMilitary method
+    //ConvertToStandard method
+    //depending on the hours, the clocktype, and hours will be changed into standard format. Since the minutes are in the same format, those do not need to be touched
     private void ConvertToStandard()
     {
-        // insert code here
+        //will only work if clocktype is military
+        if (clockType == 'M')
+        {
+            if (hours >= 12) //if the military time is > 1200 hrs, then it is no longer possible to be AM 
+            {
+                clockType = 'P';
+                hours -= 12;    //take away 12 hours, add the remainder to the hours after setting to pm
+            }
+            else   //if the military time is < 1159, then it is not possible to be PM
+            {
+                clockType = 'A';
+                //hours can stay the same
+            }
+        }
     }
-
+    /*
     // ToString method
     public override string ToString()
     {
-        return;    //!!!!!CHANGE WHEN POSSIBLE,,, PLACEHOLDER!!!!
+        //return;    //!!!!!CHANGE WHEN POSSIBLE,,, PLACEHOLDER!!!!
     }
 
     // Overloaded Operator Addition method
     public static Clock operator+(Clock clk1, Clock clk2)
     {
-        return;    //!!!!!CHANGE WHEN POSSIBLE,,, PLACEHOLDER!!!!
+        //return;    //!!!!!CHANGE WHEN POSSIBLE,,, PLACEHOLDER!!!!
     }
 
     // Greater Than operator
@@ -120,6 +168,7 @@ public class Clock
     {
         return true;    //!!!!!CHANGE WHEN POSSIBLE,,, PLACEHOLDER!!!!
     }
+     */
 }
 
     
--- ClockDemo.cs
+++ ClockDemo.cs
@@ -2,8 +2,14 @@
 
 public static class Driver
 {
-static void Main()
+    static void Main()
     {
-        
+        Clock c1 = new Clock();
+        Console.WriteLine("{0}, {1}, {2}", c1.Hours, c1.Minutes, c1.ClockType);
+
+        Clock c2 = new Clock(1, 20, 'P');
+        Console.WriteLine("{0}, {1}, {2}", c2.Hours, c2.Minutes, c2.ClockType);
+
+        Console.Read();
     }
 }
