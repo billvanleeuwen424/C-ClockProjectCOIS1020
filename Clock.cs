83692627b283271bd7147e3e614e4e0c0c41bf57
unknown <billvanleeuwen424@gmail.com>
2021-03-21 12:01:34 -0400

    Just finished the properties and the 3 param constructor

--- Clock.cs
+++ Clock.cs
@@ -9,30 +9,80 @@ public class Clock
     // No Parameter Constructor
     public Clock()
     {
-        // insert code here
+        //set default. 12:00AM
+        hours = 12;
+        minutes = 0;
+        clockType = 'a';
     }
 
     // Three Parameter Constructor
     public Clock(int hrs, int mins, char cType)
     {
-        // insert code here
+        ClockType = cType;
+        Hours = hrs;
+        Minutes = mins;
+        
     }
 
     // Hours Property
     public int Hours
     {
-        // insert code here
+        get;    //default getter
+
+        set
+        {
+            if (ClockType == 'm')
+            {
+                if (value <= 23 && hours >= 0)  //checks if hours are valid 0 - 23 for military
+                    hours = value;
+                else
+                {
+                    hours = 0;
+                    Console.WriteLine("invalid time entered... Hours set to 00");
+                }
+            }
+            else
+            {
+                if (value <= 12 && hours > 0)   //checks if hours are valid 1 - 12 for standard time
+                    hours = value;
+                else
+                {
+                    hours = 12;
+                    Console.WriteLine("invalid time entered... Hours set to 12");
+                }
+            }
+        }
     }
 
     // Minutes Property
     public int Minutes
     {
-        // insert code here
+        get;
+        set
+        {
+            if (value < 59 && value >= 0)   //checks if mintues are valid. 0-59
+                minutes = value;
+            else
+            {
+                minutes = 0;
+                Console.WriteLine("invalid time entered... Minutes set to 00");
+            }
+        }
+        
     }
 
+    //Clock type property
     public char ClockType
     {
-        // insert code here
+        get;    //default getter
+
+        set //checks if value is correct, else sets type to AM
+        {
+            if (value == 'A' || value == 'P' || value == 'M')
+                clockType = value;
+            else
+                clockType = 'A';
+        }
     }
 
     //ConvertToMilitary method
